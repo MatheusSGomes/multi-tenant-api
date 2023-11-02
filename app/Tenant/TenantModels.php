c1389da38f9e794041eeee962a454a3333f94352
Matheus Gomes <math.s.gomes@gmail.com>
2023-11-01 23:24:25 -0300

    model events - creating

--- app/Tenant/TenantModels.php
+++ app/Tenant/TenantModels.php
@@ -2,11 +2,18 @@
 
 namespace App\Tenant;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+
 trait TenantModels
 {
-    protected static function boot()
+    protected static function boot(): void
     {
         parent::boot();
         static::addGlobalScope(new TenantScope());
+        static::creating(function (Model $obj) {
+            $companyId = Auth::user()->company_id;
+            $obj->company_id = $companyId;
+        });
     }
 }
